d9ae5c19ad8824ec02cf8172432c8bd7b5705e0e
Harsh Jain <105813149+HarshJain-29@users.noreply.github.com>
2022-05-29 14:00:12 +0530

    Add files via upload

---
+++ Social_media.bat
@@ -0,0 +1,6 @@
+start http://www.facebook.com
+start http://www.instagram.com
+start http://www.youtube.com
+start http://www.twitter.com
+start http://www.snapchat.com
+start http://www.whatsapp.com
